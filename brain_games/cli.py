3979209a59202f178e774fdc634e71917e5adacf
Safintim <timurtlt96@mail.ru>
2021-09-02 09:26:46 +0300

    Delete cli.py

--- brain_games/cli.py
+++
@@ -1,8 +0,0 @@
-import prompt
-
-
-def get_user_name():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
-    return name
